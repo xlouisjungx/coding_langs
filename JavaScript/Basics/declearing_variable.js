bebc4e65e47292b9d610b42755d5737484547e2f
xlouisjungx <cutaandcrazy@gmail.com>
2025-07-28 16:21:32 +0900

    README

--- JavaScript/Basics/declearing_variable.js
+++ JavaScript/Basics/declearing_variable.js
@@ -20,7 +20,15 @@ ive = '안유진';
 console.log(ive);
 
 // const는 추후 값을 변경할 수 없음.
-// const newJeans = '뉴진스';
-// console.log(newJeans);
+const newJeans = '뉴진스';
+console.log(newJeans);
 
 // newJeans = '에스파';
+
+/**
+ * 선언과 할당
+ *
+ * 1) 변수를 선언하는 것
+ * 2) 할당
+ * 3)
+ */

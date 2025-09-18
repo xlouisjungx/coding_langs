27a5d1e1bbb30de1a91d27308bfb3316f9e37967
xlouisjungx <cutaandcrazy@gmail.com>
2025-09-18 23:05:27 +0900

    호이스팅에 대해 학습하였습니다.

---
+++ JavaScript/Basics/6_hoistin.js
@@ -0,0 +1,26 @@
+/**
+ * Hoisting
+ */
+console.log('Hello');
+console.log('World');
+console.log('---------');
+
+// console.log(name);
+// var name = 'songhyunsik';
+// console.log(name);
+
+/**
+ * Hoisting은 무엇인가?
+ *
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상을 이야기한다.
+ * Let과 const도 됨.
+ */
+
+// var name;
+// console.log(name);
+// name = 'songhyunsik';
+// console.log(name);
+
+console.log(yuJin);
+//let yuJin = '안유진';
+const yuJin = '안유진';
